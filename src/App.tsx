3000b07e8f11b835bb5e01c8f6c924556fb9bcee
MKukarev <misharielt@gmail.com>
2021-04-15 14:01:43 +0300

    change title

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import logo from "./logo.svg";
 
 function App() {
-	return <h1 style={{ textAlign: "center" }}>First commit</h1>;
+	return <h1 style={{ textAlign: "center" }}>Drive. First commit</h1>;
 }
 
 export default App;
