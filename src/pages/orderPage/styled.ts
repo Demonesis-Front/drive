1853e2630fe4eb61c4c86c686aa108d00c80f080
MKukarev <misharielt@gmail.com>
2021-04-27 16:52:05 +0300

    fixed model layout

--- src/pages/orderPage/styled.ts
+++ src/pages/orderPage/styled.ts
@@ -94,10 +94,8 @@ export const StageContentContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
 
-  @media (max-width: 1200px) {
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
+  @media (max-width: 1310px) {
+    grid-template-columns: 1fr;
   }
 
   @media (max-width: 1000px) {
